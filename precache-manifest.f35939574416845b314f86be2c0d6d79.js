a1bc55448da6098d9adbf744fedbcefc090d59c0
dyl-kh <dylanjkhan@hotmail.com>
2022-07-26 12:37:51 +1000

    Updates

---
+++ precache-manifest.f35939574416845b314f86be2c0d6d79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "98581524402867ed8ab3",
+    "url": "/memorygame/static/css/main.67ea7ae4.chunk.css"
+  },
+  {
+    "revision": "98581524402867ed8ab3",
+    "url": "/memorygame/static/js/main.98581524.chunk.js"
+  },
+  {
+    "revision": "844faec30c80640ad92b",
+    "url": "/memorygame/static/js/1.844faec3.chunk.js"
+  },
+  {
+    "revision": "1f0724a0a392b4ba4d33",
+    "url": "/memorygame/static/js/runtime~main.1f0724a0.js"
+  },
+  {
+    "revision": "1d101d46f04c18739dafbf2b9e252488",
+    "url": "/memorygame/static/media/circle.1d101d46.svg"
+  },
+  {
+    "revision": "56411e4a18dd37fb3185519a3b04c84d",
+    "url": "/memorygame/static/media/clock.56411e4a.svg"
+  },
+  {
+    "revision": "7d931a707f95f1a47a9650622936977f",
+    "url": "/memorygame/static/media/hexagon.7d931a70.svg"
+  },
+  {
+    "revision": "3d9374a2c1c01153ad9b221b334087ac",
+    "url": "/memorygame/static/media/water.3d9374a2.svg"
+  },
+  {
+    "revision": "18bf8878c3aa88c04502f5b84670e0ac",
+    "url": "/memorygame/static/media/pig.18bf8878.svg"
+  },
+  {
+    "revision": "76200e08f201a68ccb6db6c6caea9f0b",
+    "url": "/memorygame/static/media/rocket.76200e08.svg"
+  },
+  {
+    "revision": "ee7f0341447ab17b78d00ace68a8c25b",
+    "url": "/memorygame/static/media/square.ee7f0341.svg"
+  },
+  {
+    "revision": "ef077f5c1c55ada9977f4f578ca1c5f4",
+    "url": "/memorygame/static/media/triangle.ef077f5c.svg"
+  },
+  {
+    "revision": "5256ea61518edf4aecd3be39829e570b",
+    "url": "/memorygame/index.html"
+  }
+];
